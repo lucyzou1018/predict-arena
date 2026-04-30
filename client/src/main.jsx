0de40276139926789dc9a9709eacd45ae2506242
zoulihong1018 <141053996+zoulihong1018@users.noreply.github.com>
2026-04-30 14:53:31 +0800

    Add Vercel web analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,2 +1,3 @@
-import React from"react";import ReactDOM from"react-dom/client";import App from"./App";import"./styles/global.css";
+import React from"react";import ReactDOM from"react-dom/client";import{inject}from"@vercel/analytics";import App from"./App";import"./styles/global.css";
+inject();
 ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><App/></React.StrictMode>);
